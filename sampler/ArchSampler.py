59a3a5b5dd826861372e64f170dbeeefd3f53c35
AlbertiPot <1071794833@qq.com>
2021-09-19 14:02:24 +0000

    finish part of sampler, seq2matirx remained

--- sampler/ArchSampler.py
+++ sampler/ArchSampler.py
@@ -1,58 +1,157 @@
 """
-random包: https://docs.python.org/3/library/random.html
+Data: 2021/09/18
+Target: Sample subnets under FLOPS and Parameters contraints
+Method: 1 sample target FLOPS and Parameters constraints according to top tier distribution
+        2 sample n_nodes according to top tier distribution
+        3 Given the sampled n_nodes, sample edges and nodes type according to uniform distribution
+        4 Check whether the sampled subnets satisfy the FLOPS and Parameters constraints
+
+        random包: https://docs.python.org/3/library/random.html
+
+input: 1 batch_statics_dict = 
+        {
+            'flops':[{tier1},{tier2},{tier3},{tier4},{tier5}]
+            'params':[{tier1},{tier2},{tier3},{tier4},{tier5}]
+            'n_nodes':[{tier1},{tier2},{tier3},{tier4},{tier5}]
+            } # tier是counts_dict
 """
-
-from prob_calculating import compute_kl_div
-
 import random
+from prob_calculating import select_distri, extract_dict_value_to_list
 
-def extract_dict_value_to_list(prob_dict):
-    assert type(prob_dict) == dict, 'Not a dictionary'
-    return list(prob_dict.values())
+def sample_helper(disti_dict):
+    target_list = extract_dict_value_to_list(disti_dict, is_key= True)      # 提取计数, key
+    prob_list = extract_dict_value_to_list(disti_dict)                      # 提取概率，value
+    return target_list, prob_list
 
-def select_distri(list_candi, n_tier, threshold_kl_div, batch_size, batch_factor):
-# 先确定分布的样本数量是否足够多，再确定候选分布与差结构的分布的相似性
-# threahold_kl_div的应该随着batch增加越来越大，因为后期5个层级的分布可能趋同
-    for i in range(n_tier):
-        candi = list_candi[i]                                   # 提取list存储的counts_dict作为candidates
-        if sum(candi.values()) < batch_factor * batch_size:     # 若candi的计数小于batch的batch_factor时，这个分布不能用，继续遍历第二个
-            continue
+class ArchSampler(object):
+    def __init__(self, n_tier, threshold_kl_div, batch_size, batch_factor, node_type_dict, max_edges = 9, reuse_step=None):
+        self.n_tier = n_tier
+        self.threshold_kl_div = threshold_kl_div
+        self.batch_size = batch_size
+        self.batch_factor = batch_factor
+        self.reuse_step = reuse_step
+        self.node_type_dict = node_type_dict        # type_dict = {'input':1, 'conv1x1-bn-relu':2 , 'conv3x3-bn-relu':3, 'maxpool3x3':4, 'output':5}
+        self.max_edges = max_edges
+
+    def reset_parameters(self, batch_statics_dict):
+        self.batch_flops_list = batch_statics_dict['flops']
+        self.batch_params_list = batch_statics_dict['params']
+        self.batch_n_nodes_list = batch_statics_dict['n_nodes']
+
+    def _sample_target_value(self, candi_list, kl_div_factor=1):
+        threshold_kl_div = self.threshold_kl_div * kl_div_factor
+        target_distri = select_distri(candi_list, self.n_tier, threshold_kl_div, self.batch_size, self.batch_factor)
         
-        candi_counts = extract_dict_value_to_list(candi)
-        
-        for j in range(n_tier, len(list_candi)):
-            low_tier_counts = extract_dict_value_to_list(list_candi[j])
-            if compute_kl_div(low_tier_counts, candi_counts) < threshold_kl_div:        # 送两个原始计数序列去计算
-                # 当差结构的分布与candi的分布相近时，表明candi分布不能代表优秀的结构
-                return None
+        if target_distri is not None:
+            target_list, prob_list = sample_helper(target_distri)
+            target_value = random.choices(target_list, weights=prob_list)[0]
+            return target_value
+        else:
+            target_list = extract_dict_value_to_list(candi_list[0], is_key=True)
+            target_value = random.choice(target_list)
+            return target_value
 
-        return candi
+    def _sample_edges_and_types(self, n_nodes, arch_struct_list:list):
+        type_candi_list = extract_dict_value_to_list(self.node_type_dict)[1:-1]
+
+        for node_idx in range(1, n_nodes):                            # input后的第一个点开始，到output点，为每个点决定之前的连接
+            # 1 sample previous node
+            pre_node_id = random.choice([i for i in range(node_idx)])   # 从i节点之前的i-1个节点中选取一个座连接
+            arch_struct_list.append(pre_node_id)
+
+            # 2 sample node type
+            if node_idx == n_nodes-1: # 最后一个节点是output不需要做决定
+                break
+            node_opt_type = random.choice(type_candi_list)
+            arch_struct_list.append(node_opt_type)
+            
+        # 3 sampel rest edges
+        # max edges = 9, the rest edges are (max_edges -(n_nodes-1))
+        for i in range(self.max_edges-n_nodes+1):           # 遍历剩余边的start和end节点
+            # sample begin node for the edge
+            begin_node_idx = random.choice([b_idx for b_idx in range(n_nodes)])
+            if begin_node_idx == n_nodes-1:   # if begin node is the output node, this edge does not exist
+                arch_struct_list += [None, None]
+                continue
+            
+            # sample end node for the edge
+            end_node_idx = random.choice([l_idx for l_idx in range(begin_node_idx, n_nodes)])
+            if end_node_idx == begin_node_idx:
+                arch_struct_list += [None, None]
+                continue
+
+            arch_struct_list += [begin_node_idx, end_node_idx]
+
+        # 1st for loop: (n_nodes-1)times each add 2(edge and opt type) excep the last output node, yield (n_nodes-1)*2-1
+        # 2nd for loop: (max_edges-n_nodes+1)times each add 2(start and end nodes), yield (max_edges-n_nodes+1)*2
+        # n_nodes stored in the first place of the list, yield 1
+        # total (n_nodes-1)*2-1+(max_edges-n_nodes+1)*2+1
+        assert len(arch_struct_list) == self.max_edges * 2, 'Wrong length of sampled arch_struct_list'
+        return arch_struct_list
+
+    def sample_subnets(self, batch_statics_dict, n_subnets, kl_factor_list=[1,1,1]):
+        self.reset_parameters(batch_statics_dict)       # update batch statics with new batch
+        flops_kl_factor = kl_factor_list[0]
+        params_kl_factor = kl_factor_list[1]
+        n_nodes_kl_factor = kl_factor_list[2]
+
+        flops, params, n_nodes = 0,0,0
+
+        self.reuse_step = 1 if self.reuse_step is None else self.reuse_step
+        assert self.reuse_step > 0, 'the reuse step must greater than 0'
+        for i in range(n_subnets):
+            # FLOPS和params可重复若干个采样过程使用，提高采样速度
+            # step1 sample flops and params constraints
+            if i % self.reuse_step == 0:
+                # sample target flops
+                batch_flops_list = self.batch_flops_list
+                flops = self._sample_target_value(batch_flops_list, flops_kl_factor)
+
+                # sample target params
+                batch_params_list = self.batch_params_list
+                params = self._sample_target_value(batch_params_list, params_kl_factor)
+
+            arch_struct_list = []   # store arch
+            
+            # step2 sample target n_nodes
+            # n_nodes_list一个cell总的节点数, 也即是包括input和output节点的
+            batch_n_nodes_list = self.batch_n_nodes_list
+            n_nodes = self._sample_target_value(batch_n_nodes_list,n_nodes_kl_factor)
+            arch_struct_list.append(n_nodes)
+            
+            # step4 sample nodes type and connectoin
+            arch_struct_list = self._sample_edges_and_types(n_nodes, arch_struct_list)       # 这里的节点要包括input和output节点数
+            
+        return flops, params, arch_struct_list
+    
+        
 
 if __name__ == '__main__':
-    # seq = []
-    # for i in range(10):
-    #     seq.append(random.randint(0,100))
-    # prob = []
-    # for j in range(10):
-    #     prob.append(random.random())
 
-    # a = random.choices(seq, weights=prob)
-    # print(a)
-
-
-
-    # # 1.for debug extract_dict_value_to_list        #########
-    counts_dict = {5758991: 232, 11517982: 93, 17276973: 38, 23035964: 31, 28794955: 8, 34553946: 18, 40312937: 2, 46071928: 1}
-    counts_dict2 = {5758991: 1, 11517982: 2, 17276973:18, 23035964: 8, 28794955: 31, 34553946: 38, 40312937: 93, 46071928: 232}
-    # # 直接用counts送入kl散度，kl散度函数中用log_softmax归一就好
-    # # counts_dict = convert_count_to_prob(counts_dict)
-    # # counts_dict2 = convert_count_to_prob(counts_dict2)
-    # counts_list = extract_dict_value_to_list(counts_dict)
-    # counts_list2 = extract_dict_value_to_list(counts_dict2)
-    # v = compute_kl_div(counts_list,counts_list2)    
+    # for test _sample_target_value
+    # import copy
+    # counts_dict = {5758991: 232, 11517982: 93, 17276973: 38, 23035964: 31, 28794955: 8, 34553946: 18, 40312937: 2, 46071928: 1}
+    # counts_dict2 = {5758991: 1, 11517982: 2, 17276973:18, 23035964: 8, 28794955: 31, 34553946: 38, 40312937: 93, 46071928: 232}
+    # counts_dict3 = copy.deepcopy(counts_dict)
+    # counts_dict4 = copy.deepcopy(counts_dict2)
+    # counts_dict5 = copy.deepcopy(counts_dict)
+    # counts_dict6 = copy.deepcopy(counts_dict2)
+    # list_candi1 = [counts_dict,counts_dict2]
+    # list_candi2 = [counts_dict3,counts_dict4]
+    # list_candi3 = [counts_dict5,counts_dict6]
+    # candi_dict = {}
+    # candi_dict['flops'] = list_candi1
+    # candi_dict['params'] = list_candi2
+    # candi_dict['n_nodes'] = list_candi3
+    # sampler = ArchSampler(n_tier=1,threshold_kl_div=27,batch_size=423,batch_factor=1,reuse_step=10)
+    # v = sampler.sample_subnets(candi_dict, 100, kl_factor_list=[1,1,1])
     # print(v)
-
-    # 2.for debug select_distri                   #########
-    list_candi = [counts_dict,counts_dict2]
-    v = select_distri(list_candi,1,27,423,0.1)
+    
+    #  for test __sample_edges_and_types
+    type_dict = {'input':1, 'conv1x1-bn-relu':2 , 'conv3x3-bn-relu':3, 'maxpool3x3':4, 'output':5}
+    sampler = ArchSampler(n_tier=1,threshold_kl_div=27,batch_size=423,batch_factor=1,reuse_step=10,node_type_dict=type_dict)
+    v = sampler._sample_edges_and_types(7,[66])
     print(v)
+
+    
+    
--- sampler/prob_calculating.py
+++ sampler/prob_calculating.py
@@ -48,43 +48,52 @@ def build_counts_dict(raw_list, batch_min, batch_max, bins=8):
 
     return counts_dict
 
-def extract_dict_value_to_list(prob_dict):
-    assert type(prob_dict) == dict, 'Not a dictionary'
-    return list(prob_dict.values())
+def extract_dict_value_to_list(target_dict, is_key=False):
+    assert type(target_dict) == dict, 'Not a dictionary'
+    if is_key:
+        return list(target_dict.keys())
+    return list(target_dict.values())
 
-def select_distri(list_candi, n_tier, threshold_kl_div, batch_size, batch_factor):
+def select_distri(candi_list, n_tier, threshold_kl_div, batch_size, batch_factor):
 # 先确定分布的样本数量是否足够多，再确定候选分布与差结构的分布的相似性
 # threahold_kl_div的应该随着batch增加越来越大，因为后期5个层级的分布可能趋同
+# 输入[{tier1},{tier2},{tier3},{tier4},{tier5}] list of counts_dict
+# 输出None or prob_dict
+    assert n_tier < len(candi_list), 'The candidates tier indexs should be smaller than the length of candidates list'
     for i in range(n_tier):
-        candi = list_candi[i]                                   # 提取list存储的counts_dict作为candidates
+        candi = candi_list[i]                                   # 提取list存储的counts_dict作为candidates
         if sum(candi.values()) < batch_factor * batch_size:     # 若candi的计数小于batch的batch_factor时，这个分布不能用，继续遍历第二个
             continue
         
         candi_counts = extract_dict_value_to_list(candi)
-        for j in range(n_tier, len(list_candi)):
-            low_tier_counts = extract_dict_value_to_list(list_candi[j])
+        for j in range(n_tier, len(candi_list)):
+            low_tier_counts = extract_dict_value_to_list(candi_list[j])
             if compute_kl_div(low_tier_counts, candi_counts) < threshold_kl_div:        # 送两个原始计数序列去计算
                 # 当差结构的分布与candi的分布相近时，表明candi分布不能代表优秀的结构
                 return None
 
-        return candi
+        return convert_count_to_prob(candi)
 
 if __name__ == '__main__':
 
     # # 1.for debug build_counts_dict&convert_count_to_prob         ########
-    # filename = "/home/ubuntu/workspace/nar/data/nasbench101/nasbench_only108_423.json"
-    # import json
-    # with open(filename,'r') as f:
-    #     dataset = json.load(f)
-    # f.close()
-    # params_list = []
-    # for _,subnet in enumerate(dataset):
-    #     params_list.append(subnet['trainable_parameters'])
-    # max_p, min_p = max(params_list), min(params_list)
-    # l1 = build_counts_dict(params_list, batch_min = min_p, batch_max = max_p)
-    # print(l1)
-    # l2 = convert_count_to_prob(l1)
-    # print(l2)
+    filename = "/home/ubuntu/workspace/nar/data/nasbench101/nasbench_only108_423.json"
+    import json
+    with open(filename,'r') as f:
+        dataset = json.load(f)
+    f.close()
+    params_list = []
+    for _,subnet in enumerate(dataset):
+        params_list.append(subnet['trainable_parameters'])
+    max_p, min_p = max(params_list), min(params_list)
+    l1 = build_counts_dict(params_list, batch_min = min_p, batch_max = max_p)
+    print(l1)
+    l2 = convert_count_to_prob(l1)
+    print(l2)
+    l3 = extract_dict_value_to_list(l2)
+    print(l3)
+    l4 = extract_dict_value_to_list(l2,is_key=True)
+    print(l4)
 
     # # 2.for debug extract_dict_value_to_list        #########
     counts_dict = {5758991: 232, 11517982: 93, 17276973: 38, 23035964: 31, 28794955: 8, 34553946: 18, 40312937: 2, 46071928: 1}
@@ -99,5 +108,5 @@ if __name__ == '__main__':
 
     # # 3.for debug select_distri                   #########
     # list_candi = [counts_dict,counts_dict2]
-    # v = select_distri(list_candi,1,30,423,0.1)
+    # v = select_distri(list_candi,1,21,423,0.1)
     # print(v)
