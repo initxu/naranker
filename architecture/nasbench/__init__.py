700d2e46e7a2a2706a65d69f078133c50c55d5a9
AlbertiPot <1071794833@qq.com>
2021-09-21 03:10:35 +0000

    add nasbench101 api

---
+++ architecture/nasbench/__init__.py
@@ -0,0 +1,10 @@
+"""
+Data: 2021/09/21
+Target: For NASBench101 Dataset architecture query
+Modified from https://github.com/google-research/nasbench
+Apache License 2.0: https://github.com/google-research/nasbench/blob/master/LICENSE
+"""
+
+from model_spec import ModelSpec
+
+__all__ = [ModelSpec]
