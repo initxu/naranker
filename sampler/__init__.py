ca0088da1dd9c3ac90841ccb397871513ecbc193
AlbertiPot <1071794833@qq.com>
2021-10-12 04:03:10 +0000

    pre finish arch_sampler201

--- sampler/__init__.py
+++ sampler/__init__.py
@@ -4,5 +4,6 @@ Target: 结构采样器, 概率和分布计算相关
 """
 
 from .arch_sampler import ArchSampler
+from .arch_sampler201 import ArchSampler201
 
-__all__ = [ArchSampler]
+__all__ = [ArchSampler, ArchSampler201]
