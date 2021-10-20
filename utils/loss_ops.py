4e66778f61d582d3023dee7240bc305cef936588
AlbertiPot <1071794833@qq.com>
2021-10-20 16:20:54 +0800

    add pair loss

--- utils/loss_ops.py
+++ utils/loss_ops.py
@@ -1,24 +1,56 @@
 """
 Date: 2021/09/23
 Target: loss for transformer
-
-implementation adapted from Slimmable: https://github.com/JiahuiYu/slimmable_networks.git
 """
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class CrossEntropyLossSoft(torch.nn.modules.loss._Loss):
-    """ inplace distillation for image classification """
+
+class CrossEntropyLossSoft(nn.Module):
+    """Modified from Slimmable: https://github.com/JiahuiYu/slimmable_networks.git"""
     def forward(self, output, target):
         output_log_prob = F.log_softmax(output, dim=1)              # [b_sz, n_class]
         target = target.unsqueeze(1)                                # [b_sz, 1, n_class]
         output_log_prob = output_log_prob.unsqueeze(2)              # [b_sz, n_class, 1]
         cross_entropy_loss = -torch.bmm(target, output_log_prob)    # [b_sz, 1, n_class]*[b_sz, n_class, 1)=[b_sz, 1]
+        
         return cross_entropy_loss.mean()                            # 求batch loss的平均
 
-class GlobalClassDistanceRegularization(nn.Module):
+
+class RankLoss(nn.Module):
+    """modified from ReNAS: https://github.com/huawei-noah/Efficient-Computing.git"""
+    def forward(self, outputs, labels):
+        output = outputs.unsqueeze(1)
+        output1 = output.repeat(1,outputs.shape[0])
+        label = labels.unsqueeze(1)
+        label1 = label.repeat(1,labels.shape[0])
+        tmp = (output1-output1.t())*torch.sign(label1-label1.t())   # 行代表每一个预测精度与batch中其他预测精度的对比，对角线为自己与自己对比=0
+        tmp = torch.log(1+torch.exp(-tmp))                          # hinge function
+        eye_tmp = tmp*torch.eye(len(tmp)).cuda(device=outputs.device)
+        new_tmp = tmp - eye_tmp
+        loss = torch.sum(new_tmp)/(outputs.shape[0]*(outputs.shape[0]-1))
+
+        return loss
+
+
+class CrossEntropyLossLabelSmooth(nn.Module):
+    def forward(self, output, target, eps=0.1):
+        bz = target.size(0)
+        n_class = target.size(1)
+        
+        smoothed_labels = torch.full(size=(bz,n_class), fill_value=eps/(n_class-1)).cuda(device=target.device)
+        _, idx = torch.where(target == 1)
+        smoothed_labels.scatter_(dim=1, index=idx.unsqueeze(dim=1), value=1 - eps) # scatter待验证
+        output_log_prob = F.log_softmax(output, dim=1)                          # [b_sz, n_class]
+        smoothed_labels = smoothed_labels.unsqueeze(1)                          # [b_sz, 1, n_class]
+        output_log_prob = output_log_prob.unsqueeze(2)                          # [b_sz, n_class, 1]
+        cross_entropy_loss = -torch.bmm(smoothed_labels, output_log_prob)       # [b_sz, 1, n_class]*[b_sz, n_class, 1)=[b_sz, 1]
+        return cross_entropy_loss.mean()
+
+
+class DeepExpectation(nn.Module):
     def forward(self, output, target):
         index = torch.tensor([v+1 for v in range(target.size(1))]).cuda(output.device)
         output_prob = F.softmax(output, dim=1)
@@ -26,25 +58,3 @@ class GlobalClassDistanceRegularization(nn.Module):
         target_expec = (target*index).sum(dim=1)
 
         return (torch.norm(target_expec - output_expec))
-
-class TopRankRegularization(nn.Module):
-    def forward(self, output, score):
-        
-        output_log_prob = F.softmax(output, dim=1)
-        prob_val, prob_idx = torch.topk(output_log_prob, k=1, dim=1)
-        prob_val = prob_val.squeeze(dim=1)
-        prob_idx = prob_idx.squeeze(dim=1)
-        
-        t1_idx = torch.where(prob_idx == 0)
-        t1_pred_prob =  prob_val[t1_idx]
-        t1_gt_score = score[t1_idx]
-        
-        reg_term = 0
-        if t1_gt_score.size(0) == 0:
-            return 0
-        else:    
-            for i in range(len(t1_pred_prob)-1):
-                tmp = (t1_pred_prob[i] - t1_pred_prob[i+1]) * torch.sign(t1_gt_score[i]-t1_gt_score[i+1])
-                reg_term += torch.log(1+torch.exp(-tmp))
-
-        return reg_term
