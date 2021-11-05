892b99045d57c85b1ac6fd4b5f5719e64eb78841
AlbertiPot <1071794833@qq.com>
2021-11-05 16:05:57 +0800

    debug fixed label

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -1,5 +1,5 @@
 from .nasbench import NASBenchDataBase, NASBenchDataset
 from .nasbench201 import NASBench201DataBase, NASBench201Dataset
-from .subset import SplitSubet
+from .subset import SplitSubet, SplitSubet201
 
-__all__ = [NASBenchDataBase, NASBenchDataset, NASBench201DataBase, NASBench201Dataset, SplitSubet]
+__all__ = [NASBenchDataBase, NASBenchDataset, NASBench201DataBase, NASBench201Dataset, SplitSubet, SplitSubet201]
--- dataset/subset.py
+++ dataset/subset.py
@@ -1,20 +1,39 @@
+import torch
 from torch.utils.data import Dataset
 
 from architecture import ModelSpec
 
 
 class SplitSubet(Dataset):
-    def __init__(self, full_dataset, indices: list):
+    def __init__(self, full_dataset, indices: list, n_tier=5):
         self.full_dataset = full_dataset
         self.indices = indices
 
         self.subset = [self.full_dataset[i] for i in self.indices]
+        self._sort(n_tier)
 
     def __getitem__(self, index):
         return self.subset[index]
 
     def __len__(self):
         return len(self.indices)
+
+    def _sort(self, n_tier):
+        assert len(self.indices) >= n_tier, 'batch_sz should be larger than n_tier'
+        step = int(len(self.indices) / n_tier)
+        
+        sorted_list = sorted(enumerate(self.subset),key=lambda x:x[1][1],reverse=True)
+        
+        for i in range(n_tier):
+            if i == n_tier - 1:
+                t_list = sorted_list[i * step:]
+            else:
+                t_list = sorted_list[i * step:(i + 1) * step]
+            for idx, _ in t_list:
+                target = torch.zeros(1, n_tier)
+                target[0, i].add_(1)
+                target = tuple(target)
+                self.subset[idx] = self.subset[idx] + target
 
     # Query subnet inside the sub dataset
     def query_stats_by_spec(self, model_spec: ModelSpec):
@@ -34,3 +53,39 @@ class SplitSubet(Dataset):
             return None, None, None
 
         return arch_dict['flops'], arch_dict['trainable_parameters'], index_list_idx
+
+
+class SplitSubet201(Dataset):
+    def __init__(self, full_dataset, indices: list, n_tier=5):
+        self.full_dataset = full_dataset
+        self.indices = indices
+
+        self.subset = [self.full_dataset[i] for i in self.indices]
+
+        self._sort(n_tier)
+
+    def __getitem__(self, index):
+        return self.subset[index]
+
+    def __len__(self):
+        return len(self.indices)
+
+    def _sort(self, n_tier):
+        assert len(self.indices) >= n_tier, 'batch_sz should be larger than n_tier'
+        step = int(len(self.indices) / n_tier)
+        
+        for datatype in ['cifar10', 'imagenet16', 'cifar100']:
+
+            sorted_list = sorted(enumerate(self.subset),key=lambda x:x[1][datatype][1],reverse=True)
+            
+            for i in range(n_tier):
+                
+                if i == n_tier - 1:
+                    t_list = sorted_list[i * step:]
+                else:
+                    t_list = sorted_list[i * step:(i + 1) * step]
+                for idx, _ in t_list:
+                    target = torch.zeros(1, n_tier)
+                    target[0, i].add_(1)
+                    target = tuple(target)
+                    self.subset[idx][datatype] = self.subset[idx][datatype] + target
